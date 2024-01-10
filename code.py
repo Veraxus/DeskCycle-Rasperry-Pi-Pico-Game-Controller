246aca56f07655d5c3328a086da60a9c743a0af3
Matt van Andel <matt@mattvanandel.com>
2024-01-09 23:28:42 -0800

    Completely changed direction logic
    Now assumes 2 magnets instead of 4, but fairly accurate direction detection

--- code.py
+++ code.py
@@ -1,3 +1,11 @@
+"""
+File Name: code.py
+Author: Dutch van Andel
+Date: 2024-01-09
+Description:
+    This determines the direction and speed of a stationary bike that contains two reed sensors (at approx 75 degrees
+    from the main wheel) and two magnets attached to the wheel.
+"""
 import time
 import board
 import digitalio
@@ -5,122 +13,241 @@ import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 
-# Thresholds
-pedal_sprint_rate = 0.5  # Seconds per revolution threshold for fast pedaling
-pedal_stop_timeout = 1.5 # Timeout for no pedaling detection
-debounce_time = 0.05     # Delays for switch activation checks since reed switched bounce
-loop_wait_time = 0.01    # Delay in the main loop to conserve power
 
-# Initialize keyboard
-keyboard = Keyboard(usb_hid.devices)
+# == CONFIG VALUES ======================================
 
-# Setup the DeskCycle's reed switches
-switch1 = digitalio.DigitalInOut(board.GP16)
-switch1.direction = digitalio.Direction.INPUT
-switch1.pull = digitalio.Pull.DOWN
+debug = True  # If true, no keyboard presses, only messages
 
-switch2 = digitalio.DigitalInOut(board.GP17)
-switch2.direction = digitalio.Direction.INPUT
-switch2.pull = digitalio.Pull.DOWN
+pedal_sprint_rate = 0.3  # Seconds per interval for sprinting
+pedal_timeout = 0.8  # Seconds without a switch activation before a full stop is assumed
 
-# Setup onboard LED
+debounce_time = 0.05  # Delays for switch activation checks since reed switched bounce
+loop_wait_time = 0.01  # Delay in the main loop to conserve power
+
+min_history_length = 2  # The minimum number of transitions to remember for each path before making calculations
+max_history_length = 4  # The max number of transitions to remember for each path
+
+
+# == INITIALIZE BOARD FEATURES ==========================
+
+# Top switch output (far switch / pin output)
+input_top = digitalio.DigitalInOut(board.GP16)
+input_top.direction = digitalio.Direction.INPUT
+input_top.pull = digitalio.Pull.DOWN
+
+# Bottom switch output (near switch / socket output)
+input_bottom = digitalio.DigitalInOut(board.GP18)
+input_bottom.direction = digitalio.Direction.INPUT
+input_bottom.pull = digitalio.Pull.DOWN
+
+# Onboard LED
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
-# Track which switch was the LAST to be triggered
-last_switch1 = False
-last_switch2 = False
+# Virtual keyboard output over USB
+keyboard = Keyboard(usb_hid.devices)
 
-# Pedaling direction: True for forward, False for backward, None for stopped/unknown
-pedaling_forward = None
+# Create a dictionary for converting keycodes back to key names
+keydict = {value: name for name, value in Keycode.__dict__.items() if isinstance(value, int)}
 
-# Tracks the time the most recent switch was triggered
-last_switch_time = 0
 
-# Track which keypresses are active
+# == GLOBAL STATE =======================================
+
+# Time of last switch activation
+far_switch_ltime = 0
+near_switch_ltime = 0
+
+# Tracking whether a switch was activated
+far_switch_enabled = False
+near_switch_enabled = False
+
+# What was the last switch that turned off?
+last_switch = None
+
+# Which way is the wheel turning? (0=stopped, 1=forward, 2=reverse)
+rotation_direction = 0
+
+# For faster identification of direction changes
+flip_direction = False
+
+# Track the intervals between switch activations
+far_to_near_intervals = []
+near_to_far_intervals = []
+
+# What keys are currently being pressed
 active_keys = set()
 
-# Main loop
+
+# == Functions ==========================================
+
+def press_key(keycode):
+    """
+    Press a key, add to key state, print debug msg
+
+    Args:
+        keycode (int): A keycode for key being pressed
+    """
+    global debug, keyboard, active_keys
+
+    # Press the key
+    if not debug:
+        keyboard.press(keycode)
+
+    # Track key presses
+    active_keys.add(keycode)
+
+    # Debug message
+    print(f"Pressed " + keydict[keycode])
+
+
+def release_key(keycode):
+    """
+    Release a key, discard from key state, print debug msg
+
+    Args:
+        keycode (int): A keycode for key being released
+    """
+    global debug, keyboard, active_keys
+
+    # Press the key
+    if not debug:
+        keyboard.release(keycode)
+
+    # Track key releases
+    active_keys.discard(keycode)
+
+    # Debug message
+    print(f"Released " + keydict[keycode])
+
+
+def release_all_keys():
+    """
+    Release all keyboard keys
+    """
+    global debug, keyboard, active_keys
+
+    # Press the key
+    if not debug:
+        keyboard.release_all()
+
+    # Track key releases
+    active_keys.clear()
+
+    # Debug message
+    print(f"Released all keys")
+
+
+# == START LOOP =========================================
+
 while True:
     current_time = time.monotonic()
-    
-    # Debounce-corrected switch activation check
-    switch1_active = switch1.value and (current_time - last_switch_time > debounce_time)
-    switch2_active = switch2.value and (current_time - last_switch_time > debounce_time)
+
+    # == READ SWITCH STATES =============================
+
+    # Whether to process switch logic (happens only once as soon as a switch turns off)
+    ready_for_actions = False
+
+    # Is a switch CURRENTLY active (with debounce)
+    far_switch_is_live = input_top.value and (current_time - far_switch_ltime > debounce_time)
+    near_switch_is_live = input_bottom.value and (current_time - near_switch_ltime > debounce_time)
 
     # Debug: Turn on LED if any switch is active
-    led.value = switch1_active or switch2_active
+    led.value = far_switch_is_live or near_switch_is_live
 
-    # Determine forward/backward based on the order in which switches are triggered
-    if switch1_active and not last_switch1:
-        
-        # If switch2 was active last, we are pedalling backward
-        if last_switch2:
-            pedaling_forward = False
-            print("S1A: Detected backward pedaling")
-        # Going forward (likely from a stop)
-        else:
-            pedaling_forward = True
-            print("S1A: Detected forward pedaling from stop")
-        last_switch_time = current_time
+    # Enable switches for future action
+    if far_switch_is_live:
+        far_switch_enabled = True
+    if near_switch_is_live:
+        near_switch_enabled = True
 
+    # == SWITCH TRIGGER LOGIC ===========================
 
-    elif switch2_active and not last_switch2:
-        # If switch1 was active last, we are pedalling forward
-        if last_switch1:
-            pedaling_forward = True
-            print("S2A: Detected forward pedaling")
-        # Going backward (likely from a stop)
-        else:
-            pedaling_forward = False
-            print("S2A: Detected backward pedaling from stop")
-        last_switch_time = current_time
+    # Switch just turned off, handle switch logic
+    if far_switch_enabled and not far_switch_is_live:
+        # Track interval since the other switch
+        if last_switch == 'NEAR':
+            near_to_far_intervals.append(current_time - near_switch_ltime)
 
-    # Translate detected movement into keyboard input
-    if pedaling_forward is not None:
-        
-        # Walking forward
-        if pedaling_forward and not Keycode.W in active_keys:
-            keyboard.release(Keycode.S)
-            active_keys.discard(Keycode.S)
-            keyboard.press(Keycode.W)
-            active_keys.add(Keycode.W)
-            print("Pressed W key")
-        # Walking backward
-        elif not pedaling_forward and not Keycode.S in active_keys:
-            keyboard.release(Keycode.W)
-            active_keys.discard(Keycode.W)
-            keyboard.press(Keycode.S)
-            active_keys.add(Keycode.S)
-            print("Pressed S key")
+            # Keep history pruned to max
+            if len(near_to_far_intervals) > max_history_length:
+                near_to_far_intervals.pop(0)
 
-        # Initiate sprinting (hold down SHIFT)
-        if current_time - last_switch_time <= pedal_sprint_rate and not Keycode.SHIFT in active_keys:
-            keyboard.press(Keycode.SHIFT)
-            active_keys.add(Keycode.SHIFT)
-            print("Pressed SHIFT key")
-        # Stop sprinting (release SHIFT)
-        elif current_time - last_switch_time > pedal_sprint_rate and Keycode.SHIFT in active_keys:
-            keyboard.release(Keycode.SHIFT)
-            active_keys.discard(Keycode.SHIFT)
-            print("Released SHIFT key")
-    
-    # Wheel is not moving, release all keys
-    elif active_keys:
-            keyboard.release_all()
-            active_keys.clear()
-            print("DeskCycle is inactive.")
+        # The same switch hit twice means a change in direction
+        elif last_switch == 'FAR':
+            flip_direction = True
 
-    # Detect when pedalling stops and release all keys
-    if current_time - last_switch_time >= pedal_stop_timeout and active_keys:
-        keyboard.release_all()
-        active_keys.clear()
-        pedaling_forward = None
-        print("Pedaling stopped, released all keys.")
+        # Set vars
+        far_switch_ltime = current_time
+        last_switch = 'FAR'
+        far_switch_enabled = False
+        ready_for_actions = True
+        print('-- FAR')
 
-    # Update last switch states
-    last_switch1 = switch1_active
-    last_switch2 = switch2_active
+    elif near_switch_enabled and not near_switch_is_live:
+        # Track interval since the other switch
+        if last_switch == 'FAR':
+            far_to_near_intervals.append(current_time - far_switch_ltime)
 
-    # Debounce
+            # Keep history pruned to max
+            if len(far_to_near_intervals) > max_history_length:
+                far_to_near_intervals.pop(0)
+
+        # The same switch hit twice means a change in direction
+        elif last_switch == 'NEAR':
+            flip_direction = True
+
+        # Set vars
+        near_switch_ltime = current_time
+        last_switch = 'NEAR'
+        near_switch_enabled = False
+        ready_for_actions = True
+        print('–– NEAR')
+
+    # todo: Same switch hit twice? Reverse direction.
+
+    # == CALCULATE DIRECTION ============================
+
+    # Ready to handle switch-related actions
+    if ready_for_actions:
+
+        # If we already know the direction but flip was detected, just reverse
+        if rotation_direction and flip_direction:
+            rotation_direction = 2 if rotation_direction == 1 else 1
+            flip_direction = False
+
+        # Is there enough history to calculate direction?
+        elif len(far_to_near_intervals) >= min_history_length and (
+             len(near_to_far_intervals) >= min_history_length):
+
+            # Calculate averages for each path
+            avg_far_to_near = sum(far_to_near_intervals) / len(far_to_near_intervals)
+            avg_near_to_far = sum(near_to_far_intervals) / len(near_to_far_intervals)
+
+            print(f"f2n: {avg_far_to_near}")
+            print(far_to_near_intervals)
+            print(f"n2f: {avg_near_to_far}")
+            print(near_to_far_intervals)
+
+            # The shorter path tells us the direction
+            # Note: The values are consistently backwards and I don't know why. Oh well, I will accept the consistency.
+            if avg_near_to_far < avg_far_to_near:
+                rotation_direction = 2
+                print('<== Backward')
+            else:
+                rotation_direction = 1
+                print('Forward ==>')
+
+    # Detect stoppage
+    if rotation_direction:
+        if current_time > far_switch_ltime + pedal_timeout and (
+           current_time > near_switch_ltime + pedal_timeout):
+            rotation_direction = 0
+            far_switch_ltime = 0
+            near_switch_ltime = 0
+            last_switch = None
+            far_to_near_intervals = []
+            near_to_far_intervals = []
+            print('Stopped')
+
+    # Slow down the loop
     time.sleep(loop_wait_time)
