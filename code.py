8bc380f53403ec93b57bc9a05e960b6f19c077fb
Matt van Andel <matt@mattvanandel.com>
2024-01-10 00:01:15 -0800

    RC1
    Appears to working well!

--- code.py
+++ code.py
@@ -13,12 +13,11 @@ import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 
-
 # == CONFIG VALUES ======================================
 
-debug = True  # If true, no keyboard presses, only messages
+debug = False  # 1 = minimal debug, 2 = detailed info, 3 = exhaustive info
 
-pedal_sprint_rate = 0.3  # Seconds per interval for sprinting
+pedal_sprint_rate = 0.15  # Seconds per interval for sprinting
 pedal_timeout = 0.8  # Seconds without a switch activation before a full stop is assumed
 
 debounce_time = 0.05  # Delays for switch activation checks since reed switched bounce
@@ -26,7 +25,6 @@ loop_wait_time = 0.01  # Delay in the main loop to conserve power
 
 min_history_length = 2  # The minimum number of transitions to remember for each path before making calculations
 max_history_length = 4  # The max number of transitions to remember for each path
-
 
 # == INITIALIZE BOARD FEATURES ==========================
 
@@ -49,7 +47,6 @@ keyboard = Keyboard(usb_hid.devices)
 
 # Create a dictionary for converting keycodes back to key names
 keydict = {value: name for name, value in Keycode.__dict__.items() if isinstance(value, int)}
-
 
 # == GLOBAL STATE =======================================
 
@@ -89,15 +86,12 @@ def press_key(keycode):
     """
     global debug, keyboard, active_keys
 
-    # Press the key
-    if not debug:
-        keyboard.press(keycode)
-
-    # Track key presses
-    active_keys.add(keycode)
-
-    # Debug message
-    print(f"Pressed " + keydict[keycode])
+    # Press & track the key
+    if keycode not in active_keys:
+        print(f"Pressed " + keydict[keycode])
+        active_keys.add(keycode)
+        if not debug:
+            keyboard.press(keycode)
 
 
 def release_key(keycode):
@@ -110,14 +104,13 @@ def release_key(keycode):
     global debug, keyboard, active_keys
 
     # Press the key
-    if not debug:
-        keyboard.release(keycode)
+    if keycode in active_keys:
+        print(f"Released " + keydict[keycode])
+        if not debug:
+            keyboard.release(keycode)
 
     # Track key releases
     active_keys.discard(keycode)
-
-    # Debug message
-    print(f"Released " + keydict[keycode])
 
 
 def release_all_keys():
@@ -181,7 +174,8 @@ while True:
         last_switch = 'FAR'
         far_switch_enabled = False
         ready_for_actions = True
-        print('-- FAR')
+        if debug == 3:
+            print('-- FAR')
 
     elif near_switch_enabled and not near_switch_is_live:
         # Track interval since the other switch
@@ -201,7 +195,8 @@ while True:
         last_switch = 'NEAR'
         near_switch_enabled = False
         ready_for_actions = True
-        print('–– NEAR')
+        if debug == 3:
+            print('–– NEAR')
 
     # todo: Same switch hit twice? Reverse direction.
 
@@ -210,44 +205,63 @@ while True:
     # Ready to handle switch-related actions
     if ready_for_actions:
 
-        # If we already know the direction but flip was detected, just reverse
+        # If we already know the direction but flip was detected, reverse fresh
         if rotation_direction and flip_direction:
             rotation_direction = 2 if rotation_direction == 1 else 1
             flip_direction = False
+            far_to_near_intervals.clear()
+            near_to_far_intervals.clear()
+            print('() Flipping direction')
 
         # Is there enough history to calculate direction?
-        elif len(far_to_near_intervals) >= min_history_length and (
-             len(near_to_far_intervals) >= min_history_length):
+        elif (len(far_to_near_intervals) >= min_history_length
+              and len(near_to_far_intervals) >= min_history_length):
 
             # Calculate averages for each path
             avg_far_to_near = sum(far_to_near_intervals) / len(far_to_near_intervals)
             avg_near_to_far = sum(near_to_far_intervals) / len(near_to_far_intervals)
 
-            print(f"f2n: {avg_far_to_near}")
-            print(far_to_near_intervals)
-            print(f"n2f: {avg_near_to_far}")
-            print(near_to_far_intervals)
+            if debug == 2:
+                print(f"f2n avg: {avg_far_to_near}")
+                print(' ')
+                print(f"n2f avg: {avg_near_to_far}")
 
             # The shorter path tells us the direction
             # Note: The values are consistently backwards and I don't know why. Oh well, I will accept the consistency.
             if avg_near_to_far < avg_far_to_near:
                 rotation_direction = 2
-                print('<== Backward')
+                if debug == 2:
+                    print('<== Backward')
             else:
                 rotation_direction = 1
-                print('Forward ==>')
+                if debug == 2:
+                    print('Forward ==>')
+                # Hold shift to sprint?
+                if avg_far_to_near < pedal_sprint_rate:
+                    press_key(Keycode.SHIFT)
+                else:
+                    release_key(Keycode.SHIFT)
+
+        # Press the appropriate keys!
+        if rotation_direction == 1:
+            release_key(Keycode.S)
+            press_key(Keycode.W)
+        elif rotation_direction == 2:
+            release_key(Keycode.W)
+            press_key(Keycode.S)
 
     # Detect stoppage
-    if rotation_direction:
-        if current_time > far_switch_ltime + pedal_timeout and (
-           current_time > near_switch_ltime + pedal_timeout):
-            rotation_direction = 0
-            far_switch_ltime = 0
-            near_switch_ltime = 0
-            last_switch = None
-            far_to_near_intervals = []
-            near_to_far_intervals = []
-            print('Stopped')
+    if (rotation_direction
+            and current_time > far_switch_ltime + pedal_timeout
+            and current_time > near_switch_ltime + pedal_timeout):
+        print('Stopped')
+        rotation_direction = 0
+        far_switch_ltime = 0
+        near_switch_ltime = 0
+        last_switch = None
+        far_to_near_intervals = []
+        near_to_far_intervals = []
+        release_all_keys()
 
     # Slow down the loop
     time.sleep(loop_wait_time)
